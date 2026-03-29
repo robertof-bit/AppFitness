f07de42ebe250ce3120df24e7d48977ecb0b9dd3
Roberto <robertodmsf@gmail.com>
2026-03-29 08:57:36 -0300

    feat: cria componente TitHeader

---
+++ cliente/src/App.tsx
@@ -0,0 +1,14 @@
+import TitHeader from "./components/Header";
+
+
+function  App() {
+  return (
+
+    <div className="min-h-screen bg-gray-50">
+      <TitHeader />
+     
+    </div>
+  )
+}
+
+export default App
---
+++ cliente/src/components/Header.tsx
@@ -0,0 +1,15 @@
+
+// TITULO E SUBTITULO PRINCIPAL
+
+function TitHeader() {
+
+    return (
+    <div>
+        <h1 className="text-7xl font-bold text-center text-purple-700 pt-10"> LIFE FITNESS APP </h1>
+        <p className="text-2xl font-light text-center text-gray-600 pt-10"> O App que cuida de você!  </p>
+      
+    </div>
+    )
+}
+
+export default TitHeader
