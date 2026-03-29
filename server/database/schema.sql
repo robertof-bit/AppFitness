d5546c9bbb432c485efe45c436ceac7a6acc69b5
Roberto <robertodmsf@gmail.com>
2026-03-29 12:03:00 -0300

    chore: remove app.css

---
+++ server/database/schema.sql
@@ -0,0 +1,10 @@
+create table appfit (
+id serial primary key,
+nome varchar(100),
+senha varchar(255),
+email varchar(100),
+idade int,
+peso numeric,
+altura numeric,
+atividade varchar(100),
+meta varchar(100));
