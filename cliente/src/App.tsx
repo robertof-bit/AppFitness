dc30669229d86e6e6564b4b00d10fce577ec939d
Roberto <robertodmsf@gmail.com>
2026-03-29 11:06:59 -0300

    feat: conexão front-end com back-end

--- cliente/src/App.tsx
+++ cliente/src/App.tsx
@@ -1,14 +1,38 @@
 import TitHeader from "./components/Header";
+import { useState } from 'react'
 
 
 function  App() {
+
+  const [mensagem, setMensagem] = useState('')
+
+  const buscarServidor = async () => {
+    const resposta = await fetch('http://localhost:3000/health')
+    const dados = await resposta.json()
+    setMensagem(dados.status)
+
+}
+
+
   return (
 
     <div className="min-h-screen bg-gray-50">
-      <TitHeader />
-     
+      <TitHeader /> 
+    
+
+    <div className="text-center mt-10">
+      <button
+      onClick={buscarServidor}
+      className="bg-purple-600 text-white px-6 py-2 rounded-lg" >
+        Testar Conexão
+      </button>
+
+      <p className="mt-4 text-gray-700">{mensagem}</p>
+    </div>
     </div>
   )
 }
 
 export default App
+
+
